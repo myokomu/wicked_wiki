d00c85d6df2772da0d64372b4af7b50184595e61
Steven Wang <3460883@gmail.com>
2014-03-07 20:27:04 -0800

    fixed a typo

--- lib/wicked_wiki.rb
+++ lib/wicked_wiki.rb
@@ -9,7 +9,7 @@ require "wicked_wiki/diff/diff"
 module WickedWiki  
   mattr_accessor :current_user_helper;  @@current_user_helper = :current_user
   mattr_accessor :multilingual;         @@multilingual = false
-  mattr_accessor :user_class;           @@user_class = "User"in
+  mattr_accessor :user_class;           @@user_class = "User"
   mattr_accessor :auth_redirect_path
   mattr_accessor :protections
   mattr_accessor :sign_in_path  
